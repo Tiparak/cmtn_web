7b06282abbf22f2b567f1679c48cea0e712fc473
Tiparak <tiparak.moo@gmail.com>
2025-08-23 17:05:25 +0700

    Fix port conflict error

--- server/node-build.ts
+++ server/node-build.ts
@@ -1,7 +1,7 @@
 import { createServer } from './index';
 
 const app = createServer();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
